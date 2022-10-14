cc8aa9f4503f5b08b3fce38f9d6908484300374d
kripanshujoshi95 <kripanshujoshi95@gmail.com>
2022-10-14 12:32:30 +0530

    Hello

---
+++ Pro_4/ORS_Project4/src/main/java/com/rays/pro4/Exception/DuplicateRecordException.java
@@ -0,0 +1,17 @@
+package com.rays.pro4.Exception;
+
+/**
+ * DuplicateRecordException thrown when a duplicate record occurred.
+ * 
+ * @author Kripanshu Joshi
+ *
+ */
+
+public class DuplicateRecordException extends Exception {
+
+	public DuplicateRecordException(String msg){
+		super(msg);
+	}
+
+	
+}
